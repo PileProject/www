142cd68d443c502572b9076f7d477de88cc9dbf8
yamina36 <yamina0306@yahoo.co.jp>
2014-07-07 23:23:52 +0900

    7/7最新サイト

---
+++ pile drive/email.php
@@ -0,0 +1,35 @@
+<?php
+
+if($_POST){
+
+    $name = $_POST['name'];
+
+    $email = $_POST['email'];
+
+    $message = nl2br($_POST['text']);
+
+	
+	// To send HTML mail
+	$to = "tatsuyaw0c+pileweb@gmail.com";
+
+	// subject
+
+	$subject = 'From PILE';
+
+	// To send HTML mail, the Content-type header must be set
+
+	$headers  = 'MIME-Version: 1.0' . "\r\n";
+
+	$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+
+	
+
+	// Additional headers
+	$headers .= 'From: '.$name.' <'.$email.'>' . "\r\n";
+
+	// Mail it
+  	mail($to, $subject, $message, $headers);
+
+}
+
+?>
